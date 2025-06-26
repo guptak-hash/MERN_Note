ba591b121e82ee6d48aa7e659244ec871a847e6f
guptak-hash <guptakishan1123@gmail.com>
2025-06-26 11:48:53 +0530

    created login route

--- note-backend/controller/auth.controller.js
+++ note-backend/controller/auth.controller.js
@@ -1,6 +1,8 @@
+require('dotenv').config()
 const UserModel = require("../models/user.model");
 const errorHandler = require("../utils/error");
 const bcrypt = require('bcrypt')
+const jwt=require('jsonwebtoken');
 
 const signup = async (req, res, next) => {
     const { username, email, password } = req.body;
@@ -21,4 +23,27 @@ const signup = async (req, res, next) => {
 
 }
 
-module.exports = signup
+const login=async(req,res,next)=>{
+    const {email,password}=req.body;
+    try{
+        const validUser=await UserModel.findOne({email});
+        if(!validUser){
+            return next(errorHandler(404,'User not found'))
+        };
+        const validPassword=await bcrypt.compare(password,validUser.password);
+        if(!validPassword){
+            return next(errorHandler(401,'Wrong credentials.'))
+        }
+        const token=jwt.sign({id:validUser._id,email:validUser.email},process.env.JWT_SECRET);
+        const {password:pass,...rest}=validUser._doc
+        res.cookie('access_token',token).status(200).json({
+            success:true,
+            message:'Login Successfull!',
+            user:rest
+        })
+    }catch(error){
+        next(error)
+    }
+}
+
+module.exports = {signup,login}
--- note-backend/routes/auth.routes.js
+++ note-backend/routes/auth.routes.js
@@ -1,8 +1,12 @@
 const express=require('express');
-const signup = require('../controller/auth.controller');
+const {signup, login} = require('../controller/auth.controller');
 
 const router=express.Router();
 
+// signup route
 router.post('/signup',signup)
+
+// login route
+router.post('/login',login)
 
 module.exports=router
